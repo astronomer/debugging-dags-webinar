889d70e79bdb52a0108138d999e2f062a0519a7f
TJaniF <tamara.fingerlin@astronomer.io>
2023-01-26 15:16:51 +0100

    base commit

---
+++ dags/get_activity_dag.py
@@ -0,0 +1,41 @@
+from airflow.decorators import dag, task
+from pendulum import datetime, duration
+import requests
+import logging
+
+API = "https://www.boredapi.com/api/activity"
+
+# get Airflow task and processor logger
+log = logging.getLogger('airflow.task')
+
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule="@daily",
+    tags=["Activity"],
+    catchup=False,
+    dagrun_timeout=duration(hours=1)
+)
+def get_activity_dag():
+
+    @task
+    def get_activity():
+        r = requests.get(API)
+        return r.json()
+
+    @task 
+    def write_activity_to_file(response):
+        f = open("include/activity.txt", "a")
+        f.write(f"Today you will: {response['activity']}")  # fixed quotes
+        f.close()
+
+    @task 
+    def read_activity_from_file():
+        f = open("include/activity.txt", "r")
+        log.info(f.read(5))
+        f.close()
+
+    write_activity_to_file(get_activity()) >> read_activity_from_file()
+
+
+get_activity_dag()
---
+++ tests/dags/test_dag_integrity.py
@@ -0,0 +1,72 @@
+"""Test the validity of all DAGs. 
+
+This test ensures that all Dags have tags, retries set to two, 
+and no import errors. Feel free to add and remove tests."""
+
+import os
+import logging
+from contextlib import contextmanager
+import pytest
+from airflow.models import DagBag
+
+
+@contextmanager
+def suppress_logging(namespace):
+    logger = logging.getLogger(namespace)
+    old_value = logger.disabled
+    logger.disabled = True
+    try:
+        yield
+    finally:
+        logger.disabled = old_value
+
+
+def get_import_errors():
+    """
+    Generate a tuple for import errors in the dag bag
+    """
+    with suppress_logging("airflow"):
+        dag_bag = DagBag(include_examples=False)
+
+        def strip_path_prefix(path):
+            return os.path.relpath(path, os.environ.get("AIRFLOW_HOME"))
+
+        # we prepend "(None,None)" to ensure that a test object is always created even if its a no op.
+        return [(None, None)] + [
+            (strip_path_prefix(k), v.strip()) for k, v in dag_bag.import_errors.items()
+        ]
+
+
+def get_dags():
+    """
+    Generate a tuple of dag_id, <DAG objects> in the DagBag
+    """
+    with suppress_logging("airflow"):
+        dag_bag = DagBag(include_examples=False)
+
+    def strip_path_prefix(path):
+        return os.path.relpath(path, os.environ.get("AIRFLOW_HOME"))
+
+    return [(k, v, strip_path_prefix(v.fileloc)) for k, v in dag_bag.dags.items()]
+
+
+@pytest.mark.parametrize(
+    "rel_path,rv", get_import_errors(), ids=[x[0] for x in get_import_errors()]
+)
+def test_file_imports(rel_path, rv):
+    """Test for import errors on a file"""
+    if rel_path and rv:
+        raise Exception(f"{rel_path} failed to import with message \n {rv}")
+
+
+# Added test
+
+@pytest.mark.parametrize(
+    "dag_id,dag,fileloc", get_dags(), ids=[x[2] for x in get_dags()]
+)
+def test_dag_timeout_set(dag_id, dag, fileloc):
+    """
+    Test to see that all DAGs have a dagrun_timeout value set
+    """
+    assert dag.dagrun_timeout, f"{dag_id} in {fileloc} has no dagrun_timeout"
+
